bd0a27cbc51e5062869f6787c13797f6f1c83454
Eumhongin <zizon0wpxm@naver.com>
2017-11-29 16:46:01 +0900

    추천클래스 및 Buyer Class수정 Buyer 에서 정보를 입력한뒤 검색으로 넘어감. 이때 기종검색인지 요금제검색인지 검색하게함 그리고나서 사용자가 이름이나 Phone,Calling 안에있는 변수값을 입력했을경우 찾아서 표시해준다
    if문안에 검색결과를 표시해주는걸하고
    
    if문다음에 추천을 받을껀지 이야기해보고 받는다면 기준을 입력받은뒤(금액이나,화면크기나,데이터양이나 etc..) 추천해준다
    
    추천클래스는 받은 기준을가지고 설정한오차범위내의 추천을 실행한다 이때 오차범위는 seller가 설정할수있게하고 Default값은
    기준의 95%~105%사이이다.
    
    ---2012097045 엄홍인-------------------------------

--- Software_Design/src/Buyer.java
+++ Software_Design/src/Buyer.java
@@ -24,6 +24,29 @@ public class Buyer {
 
 		
 		System.out.println("고객님의 이름은 "+getName()+"이고 나이는 "+getAge()+"이며 핸드폰 번호는 "+getPhone()+" 입니다." );
+		System.out.println("+===============================================+\n\t\t원하는 메뉴를 고르세요\n\n1.기종검색\n2.요금제검색\n3.종료\n+===============================================+\n\n메뉴선택 : ");
+		if(keyboard.nextInt() == 1)
+		{
+			//1.기종검색
+			Phone_List Buyer_Phone = new Phone_List();								//폰 리스트불러와서 사용자로부터 폰기종 입력받았을떄 검색할준비.
+			Recommand Buyer_Recommand = new Recommand();							//Recommand 불러와서 마지막에 추천 받을껀지 안받을껀지 보고 추천할것
+			if(keyboard.nextLine() == //폰리스트중의이름)																	//if문안에는 사용자로부터 입력받은거랑 폰 이름같은거 확인해보고 if문 안에서 결과출력할것.
+			{
+					
+			}
+			Buyer_Recommand.Recommand("Parameter"/*받을파라미터*/);
+			
+		}
+		else if(keyboard.nextInt() == 2)
+		{
+			//2.요금제검색
+			
+		}
+		else
+		{
+			//3.종료
+		}
+		
 	}
 
 	public String getName() {
